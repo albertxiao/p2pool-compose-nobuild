0141c96d89d1582813e01c024cbbc1447d9874e9
Albert Xiao <albert.aurora@gmail.com>
2023-03-13 20:47:58 +0700

    update dockerfile

--- docker/monero/entrypoint.sh
+++ docker/monero/entrypoint.sh
@@ -7,10 +7,12 @@ fi
 exec "/monerod" \
 --data-dir /var/monero \
 --ban-list /var/monero/block.txt \
---zmq-pub=tcp://0.0.0.0:18083 --rpc-bind-ip=0.0.0.0 --rpc-bind-port 18081 \
---non-interactive --confirm-external-bind --restricted-rpc --allow-local-ip --no-igd \
+--rpc-bind-ip=0.0.0.0 --rpc-bind-port 18081 \
+--non-interactive --confirm-external-bind \
+--restricted-rpc --allow-local-ip --no-igd \
 --fast-block-sync 1 --prep-blocks-threads $(nproc) --sync-pruned-blocks --prune-blockchain --check-updates disabled \
 --disable-dns-checkpoints \
+--no-zmq \
 --in-peers 32 \
 --out-peers 32 \
 --add-priority-node node.supportxmr.com:18080 \
